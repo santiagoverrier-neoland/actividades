a76f1117ee28a9814ae91c82fbb254b684007952
Santi Acosta Verrier <santiav@gmail.com>
2026-04-27 15:41:14 -0300

    act 1 finalizada

--- act-1-condicionales.js
+++ act-1-condicionales.js
@@ -2,3 +2,14 @@
 https://docs.google.com/document/d/1CZsIf1mvf7AmKlEgkFDRuZ1Q0MyAyzJL41ePH_O2MuM/edit?tab=t.0#heading=h.jr8o7s572w9v
 */
 
+const usuario = 'admin'
+const password = 'qwerty123'
+
+let usuarioSolicitado = prompt("Indicar usuario")
+let passwordSolicitado = prompt("Indicar password")
+
+if (usuario == usuarioSolicitado && password == passwordSolicitado) {
+    console.log("Acceso concedido")
+} else {
+    console.log("Nombre de usuario o contraseña inválidos")
+}
